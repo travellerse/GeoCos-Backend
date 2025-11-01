4fe3b76839ad04ecbbf7b2a1073b7e2e37812d87
travellerse <travellers0238@outlook.com>
2025-11-01 18:24:13 +0800

    fix: 更新时区设置为推荐的IANA名称

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,11 +20,10 @@ if READ_DOT_ENV_FILE:
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = env.bool("DJANGO_DEBUG", False)
-# Local time zone. Choices are
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# though not all of them may be available with every OS.
-# In Windows, this must be set to your system time zone.
-TIME_ZONE = "UTC+8"
+# Local time zone. Use an IANA time zone name (recommended) so ZoneInfo can find it.
+# For UTC+8 (China), 'Asia/Shanghai' is a common choice. Avoid non-IANA strings like
+# 'UTC+8' which raise ZoneInfoNotFoundError.
+TIME_ZONE = "Asia/Shanghai"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#languages
